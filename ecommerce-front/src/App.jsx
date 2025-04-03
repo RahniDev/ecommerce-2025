84d8860f35bdc0f5d27e8468c4d3b8d32eb767f5
RahniDev <rahni32@protonmail.com>
2025-04-03 22:04:34 +0200

    Implemented admin routes

--- ecommerce-front/src/App.jsx
+++ ecommerce-front/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import AdminRoute from './auth/AdminRoute'
 import Home from './core/Home'
 
 const App = () => {
@@ -8,6 +9,11 @@ const App = () => {
       <h1>True Being Wellness</h1>
       <Routes>
         <Route path="/" element={<Home />} />
+        <AdminRoute path="/create/product" exact element={<AddProduct />} />
+        <AdminRoute path="/create/category" exact element={<AddCategory />} />
+        <AdminRoute path="/admin/orders" exact element={<Orders />} />
+        <AdminRoute path="/admin/products" exact element={<ManageProducts />} />
+        <AdminRoute path="/admin/product/update/:productId" exact element={<UpdateProduct />} />
       </Routes>
     </>
   )
---
+++ ecommerce-front/src/admin/ManageProducts.jsx
@@ -0,0 +1,5 @@
+export const ManageProducts = () => {
+  return (
+    <div>ManageProducts</div>
+  )
+}
