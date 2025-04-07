8fad8e80f7da7c926ffeadeff1048d8eebda4a30
RahniDev <rahni32@protonmail.com>
2025-04-07 21:51:17 +0200

    Connected db

--- ecommerce-back/index.js
+++ ecommerce-back/index.js
@@ -1,11 +1,17 @@
 import express from "express"
-import { MongoClient, ServerApiVersion } from "mongodb"
 import 'dotenv/config'
+import mongoose from 'mongoose'
 
 const app = express()
 
 const port = process.env.PORT || 8000
+  
+mongoose.set("strictQuery", false)
+
+mongoose.connect(process.env.DATABASE)
+.then(() => console.log('DB connected'))
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
+
