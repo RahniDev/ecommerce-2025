c99506e54dcea6ecb6243fdf285551f098ab31f9
RahniDev <rahni32@protonmail.com>
2025-04-23 09:57:08 +0200

    Create user components

--- ecommerce-front/src/App.jsx
+++ ecommerce-front/src/App.jsx
@@ -1,22 +1,27 @@
 import { Routes, Route } from 'react-router-dom'
-import AdminRoute from './auth/AdminRoute'
-import Home from './core/Home'
+import AdminRoute from './auth/AdminRoute.jsx'
+import Signin from './user/Signin.jsx'
+import Signup from './user/Signup.jsx'
+import Profile from './user/Profile.jsx'
+import Home from './core/Home.jsx'
+// import PrivateRoute from "./auth/PrivateRoute";
+
 
 const App = () => {
-
   return (
     <>
       <h1>True Being Wellness</h1>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" exact element={<Home />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/shop" element={<Shop />} />
+        {/* <PrivateRoute path="/profile/:userId" exact component={Profile} /> */}
+        {/*   <Route path="/shop" element={<Shop />} />
         <AdminRoute path="/create/product" exact element={<AddProduct />} />
         <AdminRoute path="/create/category" exact element={<AddCategory />} />
         <AdminRoute path="/admin/orders" exact element={<Orders />} />
         <AdminRoute path="/admin/products" exact element={<ManageProducts />} />
-        <AdminRoute path="/admin/product/update/:productId" exact element={<UpdateProduct />} />
+        <AdminRoute path="/admin/product/update/:productId" exact element={<UpdateProduct />} />*/}
       </Routes>
     </>
   )
